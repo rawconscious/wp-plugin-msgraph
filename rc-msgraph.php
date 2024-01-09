1fd8a6f4db13fe568a63f68127e12ce91adef51e
Manoj Rao <manoj@rawconscious.com>
2024-01-09 06:48:42 +0530

    v1.1.0

--- rc-msgraph.php
+++ rc-msgraph.php
@@ -3,14 +3,14 @@
  * Plugin Name:       RawConscious MS Graph
  * Plugin URI:        https://wp.rawconscious.com/docs/plugins/rc-msgraph
  * Description:       Middleware for Microsoft Graph API
- * Version:           1.0.0
+ * Version:           1.1.0
  * Author:            John Smith
  * Author URI:        https://author.example.com/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  */ //phpcs:ignore.
 
-define( 'RC_MSGRAPH_VERSION', '1.0.0' );
+define( 'RC_MSGRAPH_VERSION', '1.1.0' );
 define( 'RC_MSGRAPH_PREFIX', 'rc_msgraph' );
 define( 'RC_MSGRAPH_PATH', plugin_dir_path( __FILE__ ) );
 define( 'RC_MSGRAPH_URI', plugin_dir_url( __FILE__ ) );
